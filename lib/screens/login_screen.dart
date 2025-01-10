201a2140ddb2026bb8ffcf0e0872c98075756585
Yashwanth2008 <vyashwanth2003@gmail.com>
2025-01-10 17:44:52 +0530

    Changed the parent widget to stateful widget

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -2,9 +2,14 @@ import 'package:catalyst_app/screens/signup_screen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class AuthenticationPage extends StatelessWidget {
+class AuthenticationPage extends StatefulWidget {
   const AuthenticationPage({super.key});
 
+  @override
+  State<AuthenticationPage> createState() => _AuthenticationPageState();
+}
+
+class _AuthenticationPageState extends State<AuthenticationPage> {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
