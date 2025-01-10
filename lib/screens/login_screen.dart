896673222e03dc6bd4cc7d406d603e801aab7268
Yashwanth2008 <vyashwanth2003@gmail.com>
2025-01-10 15:52:43 +0530

    File name changes

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,4 +1,4 @@
-import 'package:catalyst_app/screens/signu_screen.dart';
+import 'package:catalyst_app/screens/signup_screen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
