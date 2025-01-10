e4cde9ab2108218b55b998e4312334b2eb6025c7
Yashwanth2008 <vyashwanth2003@gmail.com>
2025-01-10 12:40:19 +0530

    Started to design home screen

---
+++ lib/screens/homeScreen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Homescreen extends StatelessWidget {
+  const Homescreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text(
+          "Hello World",
+        ),
+      ),
+    );
+  }
+}
